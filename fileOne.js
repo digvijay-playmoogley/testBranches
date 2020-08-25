738a2f97b19861f54e9f04a369498af79b040b48
Digvijay <rathore.digvijay10@gmail.com>
2020-08-25 10:35:03 +0530

    first commit with two files

---
+++ fileOne.js
@@ -0,0 +1,13 @@
+/*
+ * File: fileOne.js
+ * Project: testBranches
+ * File Created: Tuesday, 25th August 2020 10:33:14 am
+ * Author: Digvijay Rathore (rathore.digvijay10@gmail.com)
+ */
+
+const b = require('./fileTwo');
+
+const a = 5;
+console.log(a);
+
+console.log(b.a);
---
+++ fileTwo.js
@@ -0,0 +1,13 @@
+/*
+ * File: fileTwo.js
+ * Project: testBranches
+ * File Created: Tuesday, 25th August 2020 10:33:22 am
+ * Author: Digvijay Rathore (rathore.digvijay10@gmail.com)
+ */
+
+
+const obj = {
+    a: 6
+};
+
+module.exports = obj;
